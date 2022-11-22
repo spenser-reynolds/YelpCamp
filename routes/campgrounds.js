759ca7bfb211c48654cf4f1773186a88d03c20db
Spenser Reynolds <spenser.reynolds@gmail.com>
2022-11-22 16:25:32 -0600

    Configured messages using flash when adding/ deleting campgrounds or reviews. Also configured flash messages for errors.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,11 +19,11 @@ const validateCampground = (req, res, next) => {
 
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
+    res.render('campgrounds/index', { campgrounds });
 }));
 
 router.get('/new', catchAsync(async (req, res) => {
-    res.render('campgrounds/new')
+    res.render('campgrounds/new');
 }));
 
 router.post('/', validateCampground, catchAsync(async (req, res, next) => {
@@ -31,28 +31,38 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     req.flash('success', 'Successfully made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`)
+    res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.get('/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
-    res.render('campgrounds/show', { campground })
+    if(!campground){
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds')
+    };
+    res.render('campgrounds/show', { campground });
 }));
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', { campground })
+    if(!campground){
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds')
+    };
+    res.render('campgrounds/edit', { campground });
 }));
 
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
-    res.redirect(`/campgrounds/${campground._id}`)
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    req.flash('success', 'Successfully updated campground!');
+    res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.delete('/:id', catchAsync(async (req, res, next) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground.')
     res.redirect('/campgrounds');
 }));
 
