9378ae763a5f666a1adc84b1af67ec5c29b81cc4
Spenser Reynolds <spenser.reynolds@gmail.com>
2022-11-22 12:52:50 -0600

    Setup public folder for custom scripts / reconfigured where scripts are. Installed session and flash (Configured both).

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const engine = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const { join } = require('path');
@@ -21,15 +23,33 @@ db.once("open", () => {
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+const sessionconfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie:{
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        // 1000 milliseconds in a second * 60 seconds * 60 minutes in hour * 24 hours in day * 7 days
+    }
+}
+app.use(session(sessionconfig))
+app.use(flash());
 
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
